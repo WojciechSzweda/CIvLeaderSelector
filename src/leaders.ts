33db05c411540ccaf995f79bf6d3f696ce6a48a3
Wojciech <wojtix144@gmail.com>
2023-01-24 15:01:24 +0100

    add id to leaders

--- src/leaders.ts
+++ src/leaders.ts
@@ -1,259 +1,323 @@
 export interface ILeader {
 	name: string
 	url: string
+	id: number
 }
 
 export const leaders: ILeader[] = [
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/9/97/Abraham_Lincoln_%28Civ6%29.png',
-		'name': 'Abraham Lincoln'
+		'name': 'Abraham Lincoln',
+		'id': 1
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/3/33/Alexander_%28Civ6%29.png',
-		'name': 'Alexander'
+		'name': 'Alexander',
+		'id': 2
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/b/b8/Amanitore_%28Civ6%29.png',
-		'name': 'Amanitore'
+		'name': 'Amanitore',
+		'id': 3
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/8/8c/Ambiorix_%28Civ6%29.png',
-		'name': 'Ambiorix'
+		'name': 'Ambiorix',
+		'id': 4
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/9/92/B%C3%A0_Tri%E1%BB%87u_%28Civ6%29.png',
-		'name': 'Bà Triệu'
+		'name': 'Bà Triệu',
+		'id': 5
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/9/96/Basil_II_%28Civ6%29.png',
-		'name': 'Basil II'
+		'name': 'Basil II',
+		'id': 6
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/0/06/Catherine_de_Medici_%28Civ6%29.png',
-		'name': 'Catherine de Medici'
+		'name': 'Catherine de Medici',
+		'id': 7
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/e/e2/Chandragupta_%28Civ6%29.png',
-		'name': 'Chandragupta'
+		'name': 'Chandragupta',
+		'id': 8
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/9/98/Cleopatra_%28Civ6%29.png',
-		'name': 'Cleopatra'
+		'name': 'Cleopatra',
+		'id': 9
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/1/14/Cyrus_%28Civ6%29.png',
-		'name': 'Cyrus'
+		'name': 'Cyrus',
+		'id': 10
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/c/cb/Dido_%28Civ6%29.png',
-		'name': 'Dido'
+		'name': 'Dido',
+		'id': 11
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/2/2f/Eleanor_of_Aquitaine_%28English%29_%28Civ6%29.png',
-		'name': 'Eleanor of Aquitaine'
+		'name': 'Eleanor of Aquitaine',
+		'id': 12
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/d/d4/Elizabeth_%28Civ6%29.png',
-		'name': 'Elizabeth'
+		'name': 'Elizabeth',
+		'id': 13
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/4/44/Frederick_Barbarossa_%28Civ6%29.png',
-		'name': 'Frederick Barbarossa'
+		'name': 'Frederick Barbarossa',
+		'id': 14
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/2/29/Gandhi_%28Civ6%29.png',
-		'name': 'Gandhi'
+		'name': 'Gandhi',
+		'id': 15
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/5/53/Genghis_Khan_%28Civ6%29.png',
-		'name': 'Genghis Khan'
+		'name': 'Genghis Khan',
+		'id': 16
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/f/ff/Gilgamesh_%28Civ6%29.png',
-		'name': 'Gilgamesh'
+		'name': 'Gilgamesh',
+		'id': 17
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/e/e8/Gitarja_%28Civ6%29.png',
-		'name': 'Gitarja'
+		'name': 'Gitarja',
+		'id': 18
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/6/68/Gorgo_%28Civ6%29.png',
-		'name': 'Gorgo'
+		'name': 'Gorgo',
+		'id': 19
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/9/94/Hammurabi_%28Civ6%29.png',
-		'name': 'Hammurabi'
+		'name': 'Hammurabi',
+		'id': 20
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/7/77/Harald_Hardrada_%28Civ6%29.png',
-		'name': 'Harald Hardrada'
+		'name': 'Harald Hardrada',
+		'id': 21
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/0/07/Hojo_Tokimune_%28Civ6%29.png',
-		'name': 'Hojo Tokimune'
+		'name': 'Hojo Tokimune',
+		'id': 22
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/f/f6/Jadwiga_%28Civ6%29.png',
-		'name': 'Jadwiga'
+		'name': 'Jadwiga',
+		'id': 23
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/7/77/Jayavarman_VII_%28Civ6%29.png',
-		'name': 'Jayavarman VII'
+		'name': 'Jayavarman VII',
+		'id': 24
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/3/3e/Jo%C3%A3o_III_%28Civ6%29.png',
-		'name': 'João III'
+		'name': 'João III',
+		'id': 25
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/b/b9/John_Curtin_%28Civ6%29.png',
-		'name': 'John Curtin'
+		'name': 'John Curtin',
+		'id': 26
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/4/4c/Julius_Caesar_%28Civ6%29.png',
-		'name': 'Julius Caesar'
+		'name': 'Julius Caesar',
+		'id': 27
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/3/32/Kristina_%28Civ6%29.png',
-		'name': 'Kristina'
+		'name': 'Kristina',
+		'id': 28
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/5/54/Kublai_Khan_%28Chinese%29_%28Civ6%29.png',
-		'name': 'Kublai Khan'
+		'name': 'Kublai Khan',
+		'id': 29
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/4/42/Kupe_%28Civ6%29.png',
-		'name': 'Kupe'
+		'name': 'Kupe',
+		'id': 30
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/7/77/Lady_Six_Sky_%28Civ6%29.png',
-		'name': 'Lady Six Sky'
+		'name': 'Lady Six Sky',
+		'id': 31
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/e/e0/Lautaro_%28Civ6%29.png',
-		'name': 'Lautaro'
+		'name': 'Lautaro',
+		'id': 32
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/7/70/Mansa_Musa_%28Civ6%29.png',
-		'name': 'Mansa Musa'
+		'name': 'Mansa Musa',
+		'id': 33
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/2/25/Matthias_Corvinus_%28Civ6%29.png',
-		'name': 'Matthias Corvinus'
+		'name': 'Matthias Corvinus',
+		'id': 34
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/9/97/Menelik_II_%28Civ6%29.png',
-		'name': 'Menelik II'
+		'name': 'Menelik II',
+		'id': 35
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/1/1e/Montezuma_%28Civ6%29.png',
-		'name': 'Montezuma'
+		'name': 'Montezuma',
+		'id': 36
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/6/68/Mvemba_a_Nzinga_%28Civ6%29.png',
-		'name': 'Mvemba a Nzinga'
+		'name': 'Mvemba a Nzinga',
+		'id': 37
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/e/ec/Nader_Shah_%28Civ6%29.png',
-		'name': 'Nader Shah'
+		'name': 'Nader Shah',
+		'id': 38
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/6/6a/Nzinga_Mbande_%28Civ6%29.png',
-		'name': 'Nzinga Mbande'
+		'name': 'Nzinga Mbande',
+		'id': 39
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/f/f2/Pachacuti_%28Civ6%29.png',
-		'name': 'Pachacuti'
+		'name': 'Pachacuti',
+		'id': 40
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/7/7c/Pedro_II_%28Civ6%29.png',
-		'name': 'Pedro II'
+		'name': 'Pedro II',
+		'id': 41
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/4/40/Pericles_%28Civ6%29.png',
-		'name': 'Pericles'
+		'name': 'Pericles',
+		'id': 42
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/8/80/Peter_%28Civ6%29.png',
-		'name': 'Peter'
+		'name': 'Peter',
+		'id': 43
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/5/59/Philip_II_%28Civ6%29.png',
-		'name': 'Philip II'
+		'name': 'Philip II',
+		'id': 44
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/3/3b/Poundmaker_%28Civ6%29.png',
-		'name': 'Poundmaker'
+		'name': 'Poundmaker',
+		'id': 45
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/c/c3/Qin_Shi_Huang_%28Civ6%29.png',
-		'name': 'Qin Shi Huang'
+		'name': 'Qin Shi Huang',
+		'id': 46
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/6/62/Ramses_%28Civ6%29.png',
-		'name': 'Ramses'
+		'name': 'Ramses',
+		'id': 47
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/c/c4/Robert_the_Bruce_%28Civ6%29.png',
-		'name': 'Robert the Bruce'
+		'name': 'Robert the Bruce',
+		'id': 48
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/5/57/Saladin_%28Civ6%29.png',
-		'name': 'Saladin'
+		'name': 'Saladin',
+		'id': 49
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/9/9c/Seondeok_%28Civ6%29.png',
-		'name': 'Seondeok'
+		'name': 'Seondeok',
+		'id': 50
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/e/eb/Shaka_%28Civ6%29.png',
-		'name': 'Shaka'
+		'name': 'Shaka',
+		'id': 51
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/9/9d/Sim%C3%B3n_Bol%C3%ADvar_%28Civ6%29.png',
-		'name': 'Simón Bolívar'
+		'name': 'Simón Bolívar',
+		'id': 52
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/8/85/Suleiman_%28Civ6%29.png',
-		'name': 'Suleiman'
+		'name': 'Suleiman',
+		'id': 53
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/b/b0/Tamar_%28Civ6%29.png',
-		'name': 'Tamar'
+		'name': 'Tamar',
+		'id': 54
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/7/70/Teddy_Roosevelt_%28Civ6%29.png',
-		'name': 'Teddy Roosevelt'
+		'name': 'Teddy Roosevelt',
+		'id': 55
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/a/af/Tokugawa_%28Civ6%29.png',
-		'name': 'Tokugawa'
+		'name': 'Tokugawa',
+		'id': 56
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/6/6a/Tomyris_%28Civ6%29.png',
-		'name': 'Tomyris'
+		'name': 'Tomyris',
+		'id': 57
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/d/d5/Trajan_%28Civ6%29.png',
-		'name': 'Trajan'
+		'name': 'Trajan',
+		'id': 58
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/5/53/Victoria_%28Civ6%29.png',
-		'name': 'Victoria'
+		'name': 'Victoria',
+		'id': 59
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/b/b0/Wilfrid_Laurier_%28Civ6%29.png',
-		'name': 'Wilfrid Laurier'
+		'name': 'Wilfrid Laurier',
+		'id': 60
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/4/44/Wilhelmina_%28Civ6%29.png',
-		'name': 'Wilhelmina'
+		'name': 'Wilhelmina',
+		'id': 61
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/7/76/Wu_Zetian_%28Civ6%29.png',
-		'name': 'Wu Zetian'
+		'name': 'Wu Zetian',
+		'id': 62
 	},
 	{
 		'url': 'https://static.wikia.nocookie.net/civilization/images/5/5a/Yongle_%28Civ6%29.png',
-		'name': 'Yongle'
+		'name': 'Yongle',
+		'id': 63
 	}
 ]
